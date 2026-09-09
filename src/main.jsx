31c589b09f0c10a711ad0b14e5364441b8432fee
felipetdreux-eng <felipetdreux@gmail.com>
2026-09-09 15:59:51 -0300

    Enable file import from anywhere in the app

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import GlobalImportDrop from './GlobalImportDrop';
 import { getActiveAccount } from './accountStorage';
 
 const rootHost = document.getElementById('root');
 const activeAccount = getActiveAccount();
 
 if (rootHost && activeAccount?.onboardingCompleted) {
-  createRoot(rootHost).render(<React.StrictMode><App /></React.StrictMode>);
+  createRoot(rootHost).render(
+    <React.StrictMode>
+      <App />
+      <GlobalImportDrop />
+    </React.StrictMode>,
+  );
 }
