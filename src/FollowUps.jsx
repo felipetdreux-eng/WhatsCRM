6c16c5eb81782fc1e75f2fc9c2cbc7d924bc86e1
felipetdreux-eng <felipetdreux@gmail.com>
2026-09-10 09:22:20 -0300

    Add lead deletion controls

--- src/FollowUps.jsx
+++ src/FollowUps.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import LeadsPage from './LeadsPage';
+import LeadDeletionManager from './LeadDeletionManager';
 import './leads-integrated.css';
 
 export default function FollowUps(props) {
-  return <LeadsPage {...props} />;
+  return (
+    <>
+      <LeadsPage {...props} />
+      <LeadDeletionManager leads={props.leads} setLeads={props.setLeads} />
+    </>
+  );
 }
